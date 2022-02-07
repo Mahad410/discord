77e90f140ea542e6691912bba318564108f8a74e
Mahad410 <mahadqureshi410@gmail.com>
2022-02-06 22:02:45 -0600

    discord

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const sideNav = document.querySelector(".sideNav");
 const overlay = document.querySelector(".overlay");
 const ham = document.querySelector(".ham");
 const x = document.querySelector(".x");
+const d = document.querySelectorAll(".do");
+
 ham.addEventListener("click", toggleHamburger);
 x.addEventListener("click", toggleHamburger);
 overlay.addEventListener("click", toggleHamburger);
@@ -15,13 +17,28 @@ const ham_men = document.querySelector('.ham>svg');
 const r_size = window.onresize = () => {
     if (window.innerWidth <= '786') {
         ham_men.style.display = 'block';
+        for (var i = 0; i < d.length; i++) {
+            d[i].innerHTML = " Download for Phone";
+        }
+
     } else {
         ham_men.style.display = 'none';
+        for (var i = 0; i < d.length; i++) {
+            d[i].innerHTML = " Download for Windows";
+        }
     }
 }
 
 if (window.innerWidth <= '786') {
     ham_men.style.display = 'block';
+    for (var i = 0; i < d.length; i++) {
+        d[i].innerHTML = ' Download for Phone';
+    }
+
 } else {
     ham_men.style.display = 'none';
+    for (var i = 0; i < d.length; i++) {
+        d[i].innerHTML = ' Download for Windows';
+    }
+
 }
